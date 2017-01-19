91f20ab19968c321a68d28fcbdff6d2e2671855d
enovz <zvonimir.biocic.email@gamil.com>
2017-01-19 18:08:34 +0100

    removes test files

--- src/app.js
+++ src/app.js
@@ -13,32 +13,34 @@ import Chart from './lib/Chart'*/
 chartModule.init();*/
 
 
-import {GraphModule} from './lib/GraphModule'
-
-let graph = [
-    [
-        ["A1", ["B1", "B2"]],
-        ["A2", ["B1"]],
-        ["A3", ["B2"]]
-    ],
-    [
-        ["B1", ["D1", "C2"]],
-        ["B2", ["C1", "D3"]]
-    ],
-    [
-        ["D1", ["C1", "C3", "C4"]],
-        ["D2", ["C1", "C2"]],
-        ["D3", ["C4"]]
-    ],
-    [
-        ["C1"],
-        ["C2"],
-        ["C3"],
-        ["C4"]
-    ]
-];
+import { GraphModule } from './lib/GraphModule'
 
 if (!window.app) {
+
+    //request from service
+    let graph = [
+        [
+            ["A1", ["B1", "B2"]],
+            ["A2", ["B1"]],
+            ["A3", ["B2"]]
+        ],
+        [
+            ["B1", ["D1", "C2"]],
+            ["B2", ["C1", "D3"]]
+        ],
+        [
+            ["D1", ["C1", "C3", "C4"]],
+            ["D2", ["C1", "C2"]],
+            ["D3", ["C4"]]
+        ],
+        [
+            ["C1"],
+            ["C2"],
+            ["C3"],
+            ["C4"]
+        ]
+    ];
+    //end request
 
     let app = {};
     app.graphModule = new GraphModule("chart", graph);
